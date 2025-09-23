99b07eaa679094ae8a3059e3023f63d614ee8f03
Krekken-0 <gagandeepsingh9094@gmail.com>
2025-09-23 18:36:31 +0530

    add and implement a neural network

---
+++ network.js
@@ -0,0 +1,82 @@
+class NeuralNetwork{
+    //NUMBER OF NEURONS IN EACH LAYER
+    constructor(neuronCounts){
+        // console.log("neuron counts", neuronCounts);
+        this.levels = [];
+        for(let i = 0; i < neuronCounts.length - 1; i++){
+            this.levels.push(new Level(
+                neuronCounts[i], neuronCounts[i + 1]
+            ))
+        }
+        // console.log("level 1 inputs", this.levels[0]);
+        
+    }
+
+    static feedForward(givenInputs, network){
+        // console.log("givenInputs", givenInputs);
+        // console.log("network levels", network.levels[0].length);
+        let outputs = Level.feedForward(
+            givenInputs, network.levels[0]
+        )
+        // console.log("outputs in nn ff", outputs);
+        for(let i = 1; i < network.levels.length; i++){
+            outputs = Level.feedForward(
+                outputs, network.levels[i]
+            )
+        }
+        return outputs
+    }
+}
+
+
+class Level{
+    //NUMBER OF INPUT AND OUTPUT NEURONS IN THAT LEVEL
+    constructor(inputCount, outputCount){
+        this.inputs = new Array(inputCount)
+        this.outputs = new Array(outputCount)
+        this.biases = new Array(outputCount)
+
+        this.weights = []
+        for(let i = 0; i < inputCount; i++){
+            this.weights[i] = new Array(outputCount)
+        }
+
+        Level.#randomize(this)
+    }
+
+    //STATIC METHODS BELONG TO THE CLASS, NOT TO THE OBJECT OR INSTANCE
+    //CREATED FROM IT
+    static  #randomize(level){
+        for(let i = 0; i < level.inputs.length; i++){
+            for(let j = 0; j < level.outputs.length; j++){
+                level.weights[i][j] = Math.random() * 2 - 1
+            }
+        }
+
+        for(let i = 0; i < level.biases.length; i++){
+            level.biases[i] = Math.random() * 2 - 1
+        }
+    }
+
+    static feedForward(givenInputs, level){
+        // console.log('level', level);
+        for(let i = 0; i < level.inputs.length; i++){
+            level.inputs[i] = givenInputs[i]
+        }
+
+        for(let i = 0; i < level.outputs.length; i++){
+            let sum = 0;
+            for(let j = 0; j < level.inputs.length; j++){
+                sum += level.inputs[j] * level.weights[j][i];
+            }
+        if( sum > level.biases[i]){
+            level.outputs[i] = 1
+        }
+        else{
+            level.outputs[i] = 0
+        }
+        }
+
+        return level.outputs
+    }
+}
